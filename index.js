23a1a89de6def8a2b029ca8fa3ec4aeb6839e72b
geizaaquino <geizaaquino@hotmail.com>
2023-08-29 09:44:03 -0300

    Projeto click color

---
+++ index.js
@@ -0,0 +1,17 @@
+
+const colors = ['Black', 'White','Red', 'Blue', 'Yellow', 'Purple']
+const btn = document.getElementById('btn')
+const color = document.querySelector('.color')
+
+btn.addEventListener('click', function(){
+    const randomColor = getRandomColor()
+    document.body.style.background = colors[randomColor]
+    //console.log(randomColor)
+
+    color.textContent = colors[randomColor]
+
+})
+
+function getRandomColor(){
+    return Math.floor(Math.random() * colors.length)
+}
